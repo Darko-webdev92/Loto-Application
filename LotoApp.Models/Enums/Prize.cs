749d11fd08989de6d1e13328ef2375f9ef85cce9
Darko-webdev92 <darko_angelovski92@yahoo.com>
2025-01-17 19:04:45 +0100

    Modify Prize enum

--- LotoApp.Models/Enums/Prize.cs
+++ LotoApp.Models/Enums/Prize.cs
@@ -1,8 +1,12 @@
-﻿namespace LotoApp.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LotoApp.Models.Enums
 {
     public enum Prize
-    {        
+    {
+        [Display(Name = "50 Dollars")]
         _50_Dollars = 3,
+        [Display(Name = "100 Dollars")]
         _100_Dollars,
         TV,
         Vocation,
